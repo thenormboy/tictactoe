354ea8b73872852721bb25d8df679b1b2213f526
Bleaze Perris <bperriscode@gmail.com>
2023-09-05 11:52:14 -0400

    created a displayBoard function that displays the game board on the webpage

--- tictactoe.js
+++ tictactoe.js
@@ -2,6 +2,7 @@ const Gameboard = (() => {
     const row = 3;
     const column = 3;
     const board = [];
+    const container = document.querySelector('.container')
 
     for (i = 0; i < row; i++) {
         board[i] = [];
@@ -17,24 +18,24 @@ const Gameboard = (() => {
         board[rowLocation][columnLocation] = player;
     }
 
+    const displayBoard = () => {
+        (Gameboard.getBoard()).forEach(element => {
+            element.forEach(content => {
+                const cell = document.createElement('div');
+                cell.textContent = content;
+                container.appendChild(cell).className = 'grid-item';
+            })
+        });
+    }
+
     return {
         getBoard,
         getCell,
-        placeToken
+        placeToken,
+        displayBoard
     }
 
 })();
-
-//const cell = () => {
-//    let value = '';
-
-//    const changeCell = (player) => {
-//        value = player.getToken();
-//    }
-
-//    return value;
-    
-//}
 
 const player = (name, token) => {
     const getName = () => name;
@@ -47,7 +48,7 @@ const GameController = (() => {
     const player1 = player("Player One", "X");
     const player2 = player("Player Two", "O");
 
-
+    Gameboard.displayBoard();
     Gameboard.placeToken(player2.getToken(), 0, 0);
     console.log(Gameboard.getBoard());
 
